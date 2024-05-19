38442e22cc061ec6d0fccfabc8879ebab720737b
adriano <adrianocorreiacesar@gmail.com>
2024-05-19 14:31:51 +0100

    feat: pg connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PropertiesModule } from './properties/properties.module';
 
 @Module({
-  imports: [PropertiesModule],
+  imports: [
+    PropertiesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      password: 'simform',
+      username: 'adriano',
+      entities: [],
+      database: 'pgWithNest',
+      synchronize: true, // don't use in prod or can lose prod data
+      logging: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
