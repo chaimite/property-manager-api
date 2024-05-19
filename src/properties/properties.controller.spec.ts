b6d32690d094da29ef601ee208f4e6b7aa51d91e
adriano <adrianocorreiacesar@gmail.com>
2024-05-19 23:45:29 +0100

    test: fix broken tests

--- src/properties/properties.controller.spec.ts
+++ src/properties/properties.controller.spec.ts
@@ -1,17 +1,46 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PropertiesController } from './properties.controller';
 import { PropertiesService } from './properties.service';
+const oneProperty = {
+  id: 'some-uuid',
+  description: 'Test property',
+  location: 'Test location',
+  status: 'Available',
+  type: 'Residential',
+  contract_begin_at: new Date(),
+  contract_ending_at: new Date(),
+};
+const propertyArray = [oneProperty];
 
 describe('PropertiesController', () => {
   let controller: PropertiesController;
+  let service: PropertiesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PropertiesController],
-      providers: [PropertiesService],
+      providers: [
+        {
+          provide: PropertiesService,
+          useValue: {
+            create: jest.fn().mockImplementation(() => {
+              Promise.resolve(propertyArray);
+            }),
+            findAllProperties: jest.fn().mockImplementation(() => {
+              Promise.resolve(propertyArray);
+            }),
+            findOneProperty: jest.fn().mockImplementation(() => {
+              Promise.resolve(oneProperty);
+            }),
+            updateOneProperty: jest.fn().mockResolvedValue(true),
+            removeOneProperty: jest.fn().mockResolvedValue(true),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<PropertiesController>(PropertiesController);
+    service = module.get<PropertiesService>(PropertiesService);
   });
 
   it('should be defined', () => {
--- src/properties/properties.service.spec.ts
+++ src/properties/properties.service.spec.ts
@@ -1,15 +1,43 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PropertiesService } from './properties.service';
+import { Repository } from 'typeorm';
+import { Property } from './entities/property.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+
+const oneProperty = {
+  id: 'some-uuid',
+  description: 'Test property',
+  location: 'Test location',
+  status: 'Available',
+  type: 'Residential',
+  contract_begin_at: new Date(),
+  contract_ending_at: new Date(),
+};
+const propertyArray = [oneProperty];
 
 describe('PropertiesService', () => {
   let service: PropertiesService;
+  let repository: Repository<Property>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PropertiesService],
+      providers: [
+        PropertiesService,
+        {
+          provide: getRepositoryToken(Property),
+          useValue: {
+            createProperty: jest.fn().mockResolvedValue(oneProperty),
+            findAllProperties: jest.fn().mockResolvedValue(propertyArray),
+            findProperty: jest.fn().mockResolvedValue(oneProperty),
+            updateProperty: jest.fn().mockResolvedValue(true),
+            removeProperty: jest.fn().mockResolvedValue(true),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<PropertiesService>(PropertiesService);
+    repository = module.get<Repository<Property>>(getRepositoryToken(Property));
   });
 
   it('should be defined', () => {
