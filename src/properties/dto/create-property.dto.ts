c3e296a5d66f40f34b31482b2b8f7c3e4e6fdbb8
adriano <adrianocorreiacesar@gmail.com>
2024-05-29 11:58:37 +0100

    feat: make fields mandatory

--- src/properties/dto/create-property.dto.ts
+++ src/properties/dto/create-property.dto.ts
@@ -1,27 +1,33 @@
-import { IsDate, IsEnum, IsString, Length } from 'class-validator';
+import { IsDate, IsEnum, IsNotEmpty, IsString, Length } from 'class-validator';
 import { PropertyType, PropertyStatus } from '../enums/property.enums';
 import { Type } from 'class-transformer';
 
 export class CreatePropertyDto {
   @IsString()
+  @IsNotEmpty()
   @Length(1, 90)
   description: string;
 
   @IsEnum(PropertyType)
+  @IsNotEmpty()
   type: PropertyType;
 
   @IsEnum(PropertyStatus)
+  @IsNotEmpty()
   status: PropertyStatus;
 
   @IsString()
+  @IsNotEmpty()
   @Length(1, 25)
   location: string;
 
   @Type(() => Date)
+  @IsNotEmpty()
   @IsDate()
   contract_begin_at: Date;
 
   @Type(() => Date)
+  @IsNotEmpty()
   @IsDate()
   contract_ending_at: Date;
 }
