3095c7c74dc05271a23599fa84e5a68984ded199
adriano <adrianocorreiacesar@gmail.com>
2024-06-30 23:40:19 +0100

    feat: return jwt from .env

--- src/auth/auth/strategy/jwt.strategy.ts
+++ src/auth/auth/strategy/jwt.strategy.ts
@@ -2,7 +2,6 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { jwtSecret } from '../../../app.module';
 import { UsersService } from '../../../users/users.service';
 
 @Injectable()
@@ -10,7 +9,7 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   constructor(private usersService: UsersService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: jwtSecret,
+      secretOrKey: process.env.JWT,
     });
   }
 
