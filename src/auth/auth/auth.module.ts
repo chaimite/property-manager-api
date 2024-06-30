97abcc5a06ad6bbe56fbcfed4e1ae6f832e1b3e1
adriano <adrianocorreiacesar@gmail.com>
2024-06-30 22:44:30 +0100

    feat: add jwt strategy

--- src/auth/auth/auth.module.ts
+++ src/auth/auth/auth.module.ts
@@ -4,17 +4,22 @@ import { AuthController } from './auth.controller';
 import { PrismaModule } from '../../../prisma/client/prisma.module';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './strategy/jwt.strategy';
+import { UsersModule } from '../../users/users.module';
+
+export const jwtSecret = 'zjP9h6ZI5LoSKCRj';
 
 @Module({
   imports: [
     PrismaModule,
     JwtModule.register({
-      secret: 'yourSecretKey', // Replace with your actual secret key
+      secret: jwtSecret, // Replace with your actual secret key
       signOptions: { expiresIn: '5m' },
     }),
     PassportModule,
+    UsersModule,
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
