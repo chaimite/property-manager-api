14a8b63376c14f925be83677a280739f88b3d9e8
adriano <adrianocorreiacesar@gmail.com>
2024-06-30 17:06:28 +0100

    feat: add User table

---
+++ prisma/migrations/20240630160522_add_token/migration.sql
@@ -0,0 +1,35 @@
+-- CreateEnum
+CREATE TYPE "TokenType" AS ENUM ('email', 'Api');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "firstName" TEXT,
+    "lastName" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Token" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "type" "TokenType" NOT NULL,
+    "emailToken" TEXT,
+    "valid" BOOLEAN NOT NULL DEFAULT true,
+    "expirtation" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Token_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Token_emailToken_key" ON "Token"("emailToken");
+
+-- AddForeignKey
+ALTER TABLE "Token" ADD CONSTRAINT "Token_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
