7df865b03b4b4d0cc75c85cbb260df66f0d583aa
adriano <adrianocorreiacesar@gmail.com>
2024-07-05 21:23:07 +0100

    feat: add strategy to google

--- src/features/login/strategy/google.strategy.module.ts
+++ src/features/login/strategy/google.strategy.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { GoogleStrategyController } from './google.strategy.controller';
 import { GoogleStrategyService } from './google.strategy.service';
 import { GoogleStrategy } from './google.strategy';
+import { PassportModule } from '@nestjs/passport';
+import { AuthModule } from '../../../auth/auth.module';
 
 @Module({
+  imports: [PassportModule.register({ defaultStrategy: 'google' }), AuthModule],
   controllers: [GoogleStrategyController],
   providers: [GoogleStrategyService, GoogleStrategy],
 })
--- src/features/login/strategy/google.strategy.ts
+++ src/features/login/strategy/google.strategy.ts
@@ -3,12 +3,13 @@ import { Strategy, VerifyCallback } from 'passport-google-oauth20';
 import { config } from 'dotenv';
 
 import { Injectable } from '@nestjs/common';
+import { AuthService } from '../../../auth/auth.service';
 
 config();
 
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
-  constructor() {
+  constructor(private readonly authService: AuthService) {
     super({
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_SECRET,
@@ -24,12 +25,18 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     done: VerifyCallback,
   ): Promise<any> {
     const { name, emails } = profile;
-    const user = {
+    const user = await this.authService.validateGoogleUser({
       email: emails[0].value,
       firstName: name.givenName,
       lastName: name.familyName,
-      accessToken,
-    };
+      // accessToken,
+    });
+    // const user = {
+    //   email: emails[0].value,
+    //   firstName: name.givenName,
+    //   lastName: name.familyName,
+    //   accessToken,
+    // };
     done(null, user);
   }
 }
